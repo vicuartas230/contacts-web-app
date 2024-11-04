9fcb2fe7434423eba128076ad5b47b831948f84a
Victor Cuartas <78028081+vicuartas230@users.noreply.github.com>
2024-11-04 10:08:32 -0500

    Fix a typo in a variable declaration

--- src/api/expandData.js
+++ src/api/expandData.js
@@ -5,7 +5,7 @@ const baseURL = process.env.REACT_APP_ENDPOINT;
 export const getFullData = async (id) => {
     const url = `${baseURL}/${id}`
 
-    cont res = await axios.get(url);
+    const res = await axios.get(url);
     console.log(res);
     return res;
 };
