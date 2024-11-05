323b8f760b2692d4eeb159d64dcf42bc12b71811
vicuartas230 <vicuartas230@gmail.com>
2024-11-04 21:02:47 -0500

    Remove if statement

--- src/api/delete.api.js
+++ src/api/delete.api.js
@@ -4,13 +4,7 @@ const baseURL = process.env.REACT_APP_ENDPOINT;
 
 export const deleteContact = async id => {
     try {
-        const res = await axios.delete(`${baseURL}/${id}`);
-
-        if (res.status != 200) {
-            throw new Error("Request failed");
-        }
-
-        return res;
+        return await axios.delete(`${baseURL}/${id}`);
     } catch (error) {
         console.error("Response error when deleting", error);
         throw error;
